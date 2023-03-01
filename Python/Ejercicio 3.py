07a7d90dae603088b4778a58b9939cc26ccadde0
KarenRios2609 <riosramirezanakaren510@gmail.com>
2023-03-01 12:02:56 -0600

    Ejercicio 3

---
+++ Python/Ejercicio 3.py
@@ -0,0 +1,32 @@
+#Desarrollar un programa que le pida al usuario su nombre, edad, precio de su carro y calcular el precio de la tenencia
+
+#nombre = input("Ingresa tu nombre: ")
+#edad = input("Ingresa tu edad: ")
+#precio = input("Ingresa el precio de tu carro: ")
+
+#precio = float(precio)
+#tenencia = 0.013
+
+#pago = precio * tenencia
+
+#print("Precio tenencia: ", pago)
+
+
+n1 = input("Ingresa el primer numero")
+n2 = input("Ingresa el segundo numero")
+n3 = input("Ingresa el tercer numero")
+
+n1 = float(n1)
+n2 = float(n2)
+n3 = float(n3)
+
+if n1 > n2 and n1 > n3:
+    print("El primer num es mayor")
+if n2 > n1 and n2 > n3:
+    print("El segundo num es mayor")
+if n3 > n1 and n3 > n2:
+    print("El tercer numero es mayor")
+    
+
+
+
