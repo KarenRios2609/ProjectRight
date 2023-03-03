1c5f492079f66f9b42deaab5fe6d746dd0eb3886
KarenRios2609 <riosramirezanakaren510@gmail.com>
2023-03-03 11:10:06 -0600

    Ejercicio 5

---
+++ Python/Ejercicio 5.py
@@ -0,0 +1,11 @@
+#Crear un programa que le pida al usuario el radio del circulo y mostrar su área
+
+radio = input("Ingresa el radio del circulo: ")
+
+radio = float(radio)
+
+r2 = radio * radio
+
+area = 3.1416 * r2
+
+print("El área del circulo es: ",area)
