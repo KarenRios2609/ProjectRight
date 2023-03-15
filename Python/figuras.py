4e4d2c298be1ee696640072f0dd2746d20cc1e90
KarenRios2609 <riosramirezanakaren510@gmail.com>
2023-03-15 12:10:26 -0600

    Figuras Python

---
+++ Python/figuras.py
@@ -0,0 +1,25 @@
+nl = input("Ingresa el numero de lados de una figura geometrica")
+nl = int(nl)
+
+if(nl == 3):
+    print("Es un triangulo")
+else:
+    if(nl == 4):
+        print("Es un cuadrado")
+    else:
+        if(nl == 5):
+            print("Es un pentagono")
+        else:
+            if(nl == 6):
+                print("Es un hexagono")
+            else:
+                if(nl == 8):
+                    print("Es un octagono")
+                else:
+                    if(nl == 10):
+                        print("Es un decagono")
+                    else:
+                        if(nl == 12):
+                            print("Es un dodecagono")
+                    
+            
